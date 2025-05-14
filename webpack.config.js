d755af5c25e7b846ee0949f346e2c8ffc502afb4
vitalii <webjs1996@gmail.com>
2025-05-15 00:23:06 +0300

    removed html loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,20 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
-let mode = 'development'
-if (process.env.NODE_ENV === 'production') {
-  mode = 'production'
-}
-
-const plugins = [
-  new HtmlWebpackPlugin({
-    template: './src/index.html',
-  }),
-]
-
 module.exports = {
-  mode,
-  plugins,
+  mode: 'development',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
   entry: './src/index.js',
   devtool: 'source-map',
   output: {
     path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
-
   devServer: {
     hot: true,
-  },
-
-  module: {
-    rules: [{ test: /\.(html)$/, use: ['html-loader'] }],
   },
 }
